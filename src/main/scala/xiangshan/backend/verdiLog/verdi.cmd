fb95a360205c421cdfbc5fb9f3c098972817e35d
xyxiayan <2435235686@qq.com>
2023-06-28 15:06:41 +0800

    8

--- src/main/scala/xiangshan/backend/verdiLog/verdi.cmd
+++ src/main/scala/xiangshan/backend/verdiLog/verdi.cmd
@@ -112,3 +112,46 @@ uniFindSearchString -widget MTB_SOURCE_TAB_1 -pattern "encei" -next
 srcDeselectAll -win $_nTrace1
 srcDeselectAll -win $_nTrace1
 uniFindSearchString -widget MTB_SOURCE_TAB_1 -pattern "encei" -next
+nsMsgSwitchTab -tab general
+debImport "/home/xiayan/share/Nanhu-V3/build/SimTop.sv" -path \
+          {/home/xiayan/share/Nanhu-V3/build}
+verdiSetActWin -dock widgetDock_<Inst._Tree>
+srcHBSelect "SimTop.l_soc.moduleInstance.intBuffer" -win $_nTrace1
+srcHBSelect "SimTop.l_soc.moduleInstance.core" -win $_nTrace1
+srcSetScope "SimTop.l_soc.moduleInstance.core" -delim "." -win $_nTrace1
+srcHBSelect "SimTop.l_soc.moduleInstance.core" -win $_nTrace1
+srcHBSelect "SimTop.l_soc.moduleInstance.core.frontend" -win $_nTrace1
+srcSetScope "SimTop.l_soc.moduleInstance.core.frontend" -delim "." -win $_nTrace1
+srcHBSelect "SimTop.l_soc.moduleInstance.core.frontend" -win $_nTrace1
+srcHBSelect "SimTop.l_soc.moduleInstance.core.frontend.icache" -win $_nTrace1
+srcSetScope "SimTop.l_soc.moduleInstance.core.frontend.icache" -delim "." -win \
+           $_nTrace1
+srcHBSelect "SimTop.l_soc.moduleInstance.core.frontend.icache" -win $_nTrace1
+srcHBSelect "SimTop.l_soc.moduleInstance.core.frontend.icache.metaArray" -win \
+           $_nTrace1
+srcSetScope "SimTop.l_soc.moduleInstance.core.frontend.icache.metaArray" -delim \
+           "." -win $_nTrace1
+srcHBSelect "SimTop.l_soc.moduleInstance.core.frontend.icache.metaArray" -win \
+           $_nTrace1
+srcDeselectAll -win $_nTrace1
+srcSelect -signal "io_fencei_done" -line 84736 -pos 1 -win $_nTrace1
+srcAction -pos 84735 3 11 -win $_nTrace1 -name "io_fencei_done" -ctrlKey off
+verdiSetActWin -dock widgetDock_MTB_SOURCE_TAB_1
+srcHBSelect "SimTop.l_soc.moduleInstance.core.frontend" -win $_nTrace1
+srcSetScope "SimTop.l_soc.moduleInstance.core.frontend" -delim "." -win $_nTrace1
+srcHBSelect "SimTop.l_soc.moduleInstance.core.frontend" -win $_nTrace1
+verdiSetActWin -dock widgetDock_<Inst._Tree>
+srcDeselectAll -win $_nTrace1
+verdiSetActWin -dock widgetDock_MTB_SOURCE_TAB_1
+verdiSetActWin -dock widgetDock_<Inst._Tree>
+srcHBDrag -win $_nTrace1
+srcHBSelect "SimTop.l_soc.moduleInstance.core.frontend.icache.meta_read_arb" -win \
+           $_nTrace1
+srcHBSelect "SimTop.l_soc.moduleInstance.core.frontend.icache.mainPipe" -win \
+           $_nTrace1
+srcHBSelect "SimTop.l_soc.moduleInstance.core.frontend.icache.dataArray" -win \
+           $_nTrace1
+srcHBSelect "SimTop.l_soc.moduleInstance.core.frontend.icache.cacheOpDecoder" \
+           -win $_nTrace1
+srcHBSelect "SimTop.l_soc.moduleInstance.core.frontend.icache.mainPipe" -win \
+           $_nTrace1

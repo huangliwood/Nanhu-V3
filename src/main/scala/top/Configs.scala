cd520fec0dfb8018977c7cc082c6bea4a8072208
zhuhang <877093922@qq.com>
2023-07-17 13:29:40 +0800

    prefetcher: add signature path prefetcher

--- src/main/scala/top/Configs.scala
+++ src/main/scala/top/Configs.scala
@@ -249,7 +249,7 @@ class WithNKBL2
         )),
         reqField = Seq(utility.ReqSourceField()),
         echoField = Seq(huancun.DirtyField()),
-        prefetch = Some(coupledL2.prefetch.PrefetchReceiverParams()),
+        prefetch = Some(coupledL2.prefetch.SPPParameters()),
         elaboratedTopDown = false,
         // prefetch = None
         // enablePerf = true,
